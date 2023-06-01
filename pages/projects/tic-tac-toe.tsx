5d4da5b3d6d60f4ad2603dce6158ccad1cdbe319
Wojciech Górczyński <woj.gorczynski@gmail.com>
2023-06-01 13:18:45 +0200

    Made tic tac toe game work

--- pages/projects/tic-tac-toe.tsx
+++ pages/projects/tic-tac-toe.tsx
@@ -2,27 +2,44 @@ import Layout from "@/components/layout";
 import Head from "next/head";
 import { useState } from "react";
 
-type Field = "X" | "O" | " "
+var player : "X" | "O" = "X"
+
+type Field = "X" | "O" | "_"
 
 export default function TicTacToe(){
+  const [fields, setFields] = useState(getEmptyBoard())
+
+  function HandleClick(at : number) : Field{
+    setFields(fields.map((field, index) => {
+      if (at === index){
+        return player
+      }
+      return field
+    }))
+    let set = player
+    player = player === "X" ? "O" : "X"
+    return set
+  }
+
     return(
         <Layout>
            <Head>
             <title> Tic Tac Toe </title>
             </Head> 
             <section>
-            <Table/>
+              <h1> Tic Tac Toe </h1>
+            <Table handler={HandleClick}/>
             </section>
         </Layout>
     )
+
+
 }
 
-function Square({ value } : { value? : Field }) {
-    if (!value) value = "X"
-    const [field, set] = useState(value)
-
-    function onClick(e : any){
-        set(field === "O" ? "X" : "O")
+function Square({ index, callback } : { index : number, callback(index : number) : Field }) {
+    const [field , setField] = useState("_" as Field)
+    function onClick(){
+        if (field === "_") setField(callback(index))
         return 
     }
 
@@ -35,17 +52,24 @@ function Square({ value } : { value? : Field }) {
 
   }
 
-function Table(){
+function Table({ handler } : {handler (index : number) : Field}){
     return(
-        <>
-        <div><Square/>
-        <Square/>
-        <Square/></div>
-        <div><Square/>
-        <Square/>
-        <Square/></div><div><Square/>
-        <Square/>
-        <Square/></div>
+      <>
+        <div>
+        <Square index={0} callback = {handler} />
+        <Square index={1} callback = {handler} />
+        <Square index={2} callback = {handler} /></div>
+        <div>
+        <Square index={4} callback = {handler} />
+        <Square index={5} callback = {handler} />
+        <Square index={6} callback = {handler} /></div>
+        <div>
+        <Square index={7} callback = {handler} />
+        <Square index={8} callback = {handler} />
+        <Square index={9} callback = {handler} /></div>
         </>
-    )
+        )
+}
+function getEmptyBoard() : Field[]{
+  return ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
 }
