10fe9e1b4b064c14f9abf343e88b26fcb946910a
Wojciech Górczyński <woj.gorczynski@gmail.com>
2023-06-06 12:01:28 +0200

    Fixed missing imports

--- pages/about-me.tsx
+++ pages/about-me.tsx
@@ -1,21 +1,8 @@
 import Layout from "@/components/layout";
-import Post from "@/components/post";
-import { PostData, getLipsum } from "@/lib/lipsum";
-import { GetStaticProps } from "next";
 
-export const getStaticProps: GetStaticProps = async () => {
-    const postData = await getLipsum();
-    return {
-      props: {
-        postData,
-      },
-    };
-  }
-
-export default function AboutMe({postData} : PostData){
+export default function AboutMe(){
     return(
         <Layout>
-            <Post postData={postData} />
         </Layout>
     )
 }
