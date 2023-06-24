9d0738e8d4c131c1052ac0d14c71ae37e9d87030
Wojciech Górczyński <woj.gorczynski@gmail.com>
2023-06-24 13:23:45 +0200

    Added text to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,12 @@ export default function Home() {
             </Flex>
             <Stack spacing={2}>
               <Heading size='2xl'> Wojciech Górczyński </Heading>
-              <Text fontSize='sm'> @gurcxynski </Text>
+              <Text fontSize='md'> @gurcxynski </Text>
+            </Stack>
+            <Stack>
+              <Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+              </Text>
             </Stack>
           </Stack>
         </Layout>
