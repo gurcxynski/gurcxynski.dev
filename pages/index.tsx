c0e3af06c6856277bfb1d445b5d697380a371b07
Wojciech Górczyński <woj.gorczynski@gmail.com>
2023-06-26 11:54:50 +0200

    Added text to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -68,7 +68,7 @@ export default function Home({ gamesData } : { gamesData : InferGetServerSidePro
             </Stack>
             <Stack>
               <Text fontSize='xl'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+              I&apos;m a young, ambitious student of software engineering, wanting to become a software developer. I create games and other simple programs for practice or as <Link href='https://fiverr.com/gurcxynski'> Fiverr orders</Link>. These are games I created and released on <Link href='https://gurcxynski.itch.io'>my itch.io page</Link>:
               </Text>
             </Stack>
             <Divider />
